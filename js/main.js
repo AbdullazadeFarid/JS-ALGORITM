0272caebb75eb6409a0206c15c3d31e6a446dc5d
AbdullazadeFarid <abdullazadefarid@mail.ru>
2024-01-06 13:14:47 +0300

    .

---
+++ js/main.js
@@ -0,0 +1,235 @@
+//////////////////////////////////          inputa daxil olan 2 ededin cemi           ///////////////////////////////////////
+
+//var a = Number(prompt()) // prompt dan daxil olunan eded string olur diye onu number elemeliyik
+//var b =  Number(prompt())
+//var total = a+b
+//console.log(total)
+
+
+/////////////////////////             Input ile daxil olunan musbet ədədlerin cemi.   ///////////////////////////////////////////
+
+// var a = Number(prompt()) // prompt dan daxil olunan eded string olur diye onu number elemeliyik
+// var b =  Number(prompt())
+
+// if(a>0 && b>0){
+//     var total =a+b ;
+// }else{
+//     alert("menfi eded daxil etme")
+// }
+// console.log(total);
+
+
+
+
+
+//////////////////////////////////      /Input ile daxil olunan ədədin sıfır,müsbət yoxsa mənfi olduğunu tapın.      //////////////////////////
+
+//  var a =  Number(prompt())
+//  if (a>0){
+// alert("Musbet edetdir")
+//  }
+//  else if (a<0){
+//     alert("Mənfi edetir");
+//  }
+//  else {
+//     alert("Sifirdir");
+//  }
+
+
+
+
+  /////////////////////////////        Input ile daxil olunan 2 eded arasinda olan en boyuk ededi tapin   ///////////////////////////
+
+//  var a = Number(prompt()) // prompt dan daxil olunan eded string olur diye onu number elemeliyik
+//  var b =  Number(prompt())
+
+//  if(a>b){
+//     alert(a)
+//  }
+//  else if (b>a){
+//     alert(b)
+//  }
+//  else {
+//     alert('beraberdiler')
+//  }
+
+
+
+
+//////////////////////////////      Input ile daxil olunan ədədin 5-e tam bolunub bolunmediyini tapın.         //////////////////////////
+
+
+
+
+// var a = Number(prompt())
+
+// if(a%5==0){
+//     alert("5 e tam bolunur")
+// }
+// else{
+//     alert("5 e tam bolunmur")
+// }
+
+
+
+///////////////////////////////        Input ile daxil olunan ədədin cüt yoxsa tək olduğunu tapın. //////////////////////////////
+
+
+// var a = Number(prompt())
+
+//  if(a%2==0){
+//      alert("cut edetdir")
+//  }
+//  else{
+//      alert("tek edetdir")
+//  }
+
+
+
+
+/////////////////////////////////////     1-100 arasındaki cut ededlerin toplamını tapin    ////////////////////////////////
+
+// var sum = 0
+
+// for (let i = 0; i <= 100; i++) {
+
+// if(i%2==0){
+
+// sum+=i;
+// }
+// }
+
+// console.log("Сумма четных чисел от 1 до 100: " + sum);
+
+
+
+
+
+
+
+
+//////////////////////////////////       Input ile daxil edilen adi 5 defe ekrana yazdir              //////////////////////////////////////////
+
+
+// var a = prompt()
+
+// for (i=0 ; i<5 ; i++){
+// alert(a)
+// }
+
+
+
+
+
+////////////////////////////////           Input ile daxil edilen adi, Input ile daxil edilen eded defe ekrana yazdir    ///////////////////////
+
+// var ad = prompt("Ad daxil et")
+
+// var say = prompt("say daxilet")
+
+// for(i=0 ; i<say ; i++){
+//     alert(ad)
+// }
+
+
+
+//////////////////////////////         1-den Input ile daxil edilen edede qeder cut ededleri tapin    //////////////////////
+
+
+// var a = prompt()
+// for(i=1 ; i<=a ; i++){
+//     if(i%2==0){
+
+//         console.log(i);
+//     }
+// }
+
+
+
+/////////////////////////             Input ile daxil edilen 5 ededden menfi olanların toplamını, cut olanlarin vurgunu tapin,  ///////////////
+
+// var sum=0
+
+// for ( i = 0 ; i<5  ; i++ ){
+// var a =Number(prompt())
+// console.log(a);
+
+// if (a<0){
+//     sum += a
+//     console.log( "neqativ ededler: "+a);
+// }
+// }
+
+// console.log("toplam:"   +sum);
+
+
+
+
+
+
+
+//5 fenden umumi ortalama cixsin. Dersler input ile daxil edilsin ve 0dan 100-e qeder qiymet verilsin. Ededi ortalam tapilsin 5 derse gore. Eger ortalama 90+ ise A, 81-90 ise B, 71-80 ise C,c 61-70 ise D, 51-60 ise E, 50den asagi ise Kesilmisiniz ekrana cixsin. Istifadeci menfi eded daxil ede bilmez !//
+
+
+// var sum = 0
+
+// for(i=0 ; i<5 ; i++){
+//     var qiymet = Number(prompt())
+//     console.log(qiymet);
+
+//     if(qiymet>=0){
+//         sum +=qiymet
+
+//         average = sum/5
+
+
+
+//     }
+// }
+// if(average>90){
+//     alert("A")
+// }
+
+// else if(average>81){
+// alert("B")
+// }
+
+// else if(average>71){
+// alert("C")
+// }
+
+// else if (average>61){
+//     alert("D")
+// }
+
+// else if (average>51){
+//     alert("E")
+// }
+// else{
+//     alert("Kesilmisiz")
+// }
+// console.log("ortag qiymet " +average);
+
+
+
+
+
+
+
+
+
+/////////////////////          Input ile 5 eded daxil edin. 7'ye beraber olanların sayini tapib ekrana yazdırin           ////////////////////
+
+// var say = 0
+
+// for(i=0 ; i<5 ; i++){
+//     var a = prompt ();
+//     if(a==7){
+//         say++
+//     }
+
+// }
+// alert(say)
+
+
+
